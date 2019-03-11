f2a6e48ac0bf796345b185bc20641b46b5f05612
Ben Mathes <mathes.ben@gmail.com>
2019-03-11 12:38:11 -0700

    form input fields

--- pyramids.js
+++ pyramids.js
@@ -1,108 +1,66 @@
-// [START apps_script_gmail_quick_start]
 /**
- * Returns the array of cards that should be rendered for the current
- * e-mail thread. The name of this function is specified in the
- * manifest 'onTriggerFunction' field, indicating that this function
- * runs every time the add-on is started.
+ * Compose trigger function that fires when the compose action is
+ * selected. Builds and returns a compose UI for inserting images.
  *
- * @param {Object} e The data provided by the Gmail UI.
+ * @param {event} e The compose trigger event object. Not used in
+ *         this example.
  * @return {Card[]}
  */
-function interactivePyramid(e) {
-  // Activate temporary Gmail add-on scopes.
-  var accessToken = e.messageMetadata.accessToken;
-  GmailApp.setCurrentMessageAccessToken(accessToken);
-
-  var messageId = e.messageMetadata.messageId;
-  var message = GmailApp.getMessageById(messageId);
-
-  // old sample code.
-  //// Get user and thread labels as arrays to enable quick sorting and indexing.
-  //var threadLabels = message.getThread().getLabels();
-  //var labels = getLabelArray(GmailApp.getUserLabels());
-  //var labelsInUse = getLabelArray(threadLabels);
-  //
-  //// Create a section for that contains all user Labels.
-  //
-  //// Construct the Minto Question form:
-  //
-  //
-  //// Create a checkbox group for user labels that are added to prior section.
-  //var checkboxGroup = CardService.newSelectionInput()
-  //  .setType(CardService.SelectionInputType.CHECK_BOX)
-  //  .setFieldName('labels')
-  //  .setOnChangeAction(CardService.newAction().setFunctionName('toggleLabel'));
-  //
-  //// Add checkbox with name and selected value for each User Label.
-  //for(var i = 0; i < labels.length; i++) {
-  //  checkboxGroup.addItem(labels[i], labels[i], labelsInUse.indexOf(labels[i])!= -1);
-  //}
-  //
-  //// Add the checkbox group to the section.
-  //section.addWidget(checkboxGroup);
-
-
-  var promptSection = CardService.newCardSection();
-
-  // Build the main card after adding the section.
-  var card = CardService.newCardBuilder()
-    .setHeader(CardService.newCardHeader())
-    .setTitle('SCQA Builder')
-    .addSection(promptSection)
-    .build();
-
-  return [card];
+function getSCQAPromptsUI(e) {
+  return [buildSCQAPromptsCard()];
 }
 
 /**
- * Updates the labels on the current thread based on
- * user selections. Runs via the OnChangeAction for
- * each CHECK_BOX created.
+ * Build a card to display images from a third-party source.
  *
- * @param {Object} e The data provided by the Gmail UI.
-*/
-function toggleLabel(e){
-  //var selected = e.formInputs.labels;
-  //
-  //// Activate temporary Gmail add-on scopes.
-  //var accessToken = e.messageMetadata.accessToken;
-  //GmailApp.setCurrentMessageAccessToken(accessToken);
-  //
-  //var messageId = e.messageMetadata.messageId;
-  //var message = GmailApp.getMessageById(messageId);
-  //var thread = message.getThread();
-  //
-  //if (selected != null){
-  //   for each (var label in GmailApp.getUserLabels()) {
-  //     if(selected.indexOf(label.getName()) != -1){
-  //        thread.addLabel(label);
-  //     }
-  //     else {
-  //       thread.removeLabel(label);
-  //     }
-  //   }
-  //}
-  //else {
-  //  for each (var label in GmailApp.getUserLabels()) {
-  //    thread.removeLabel(label);
-  //  }
-  //}
+ * @return {Card}
+ */
+function buildSCQAPromptsCard() {
+  // Get a list of image URLs to display in the UI.
+  // This function is not shown in this example.
+
+  var card = CardService.newCardBuilder();
+  var cardSection = CardService.newCardSection().setHeader('SCQA');
+  cardSection.addWidget(
+    CardService.newTextInput()
+      .setTitle("What subject are you discussing?")
+      .setFieldName('1_discussed_subject')
+      .setHint("e.g. q4 new expansion plans")
+  );
+
+  cardSection.addWidget(
+    CardService.newTextButton()
+      .setText('insert as email')
+      .setOnClickAction(CardService.newAction()
+                        .setFunctionName('insertSCQAToEmail'))
+  );
+
+
+  return card.addSection(cardSection).build();
 }
 
 /**
- * Converts an GmailLabel object to a array of strings.
- * Used for easy sorting and to determine if a value exists.
+ * Adds an image to the current draft email when the image is clicked
+ * in the compose UI. The image is inserted at the current cursor
+ * location. If any content of the email draft is currently selected,
+ * it is deleted and replaced with the image.
  *
- * @param {labelsObjects} A GmailLabel object array.
- * @return {lables[]} An array of labels names as strings.
-*/
-function getLabelArray(labelsObjects){
-  var labels = [];
-  for(var i = 0; i < labelsObjects.length; i++) {
-    labels[i] = labelsObjects[i].getName();
-  }
-  labels.sort();
-  return labels;
+ * Note: this is not the compose action, but rather an action taken
+ * when the user interacts with the compose action's corresponding UI.
+ *
+ * @param {event} e The incoming event object.
+ * @return {UpdateDraftActionResponse}
+ */
+function insertSCQAToEmail(e) {
+  var imageUrl = e.parameters.url;
+  var response = CardService.newUpdateDraftActionResponseBuilder()
+      .setUpdateDraftBodyAction(
+        CardService.newUpdateDraftBodyAction()
+          .addUpdateContent(
+            imageHtmlContent,
+            CardService.ContentType.HTML)
+          .setUpdateType(
+            CardService.UpdateDraftBodyType.IN_PLACE_INSERT))
+      .build();
+  return response;
 }
-
-// [END apps_script_gmail_quick_start]
